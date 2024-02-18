e6c4718c944ab0a77d244a550886e794509468e7
chanchaljai <chanchal123jaiswal@gmail.com>
2024-02-18 21:57:06 +0530

    login

---
+++ app/src/main/java/com/example/demo2/CatogaryActivity.java
@@ -0,0 +1,15 @@
+package com.example.demo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatogaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catogary);
+
+    }
+}
--- app/src/main/java/com/example/demo2/LoginScreen.java
+++ app/src/main/java/com/example/demo2/LoginScreen.java
@@ -3,12 +3,19 @@ package com.example.demo2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginScreen extends AppCompatActivity {
+    EditText email2, password2;
+    Button btn2;
+    TextView signup2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
     }
 }
